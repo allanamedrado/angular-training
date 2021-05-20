4b3e5c9f02e69fadda4d6024309263a1ce987ff6
Allana Medrado <allanamedradoo@gmail.com>
2021-05-20 14:52:52 -0300

    projeto tour of heroes tutorial angular

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,7 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Dr Stranger'},
+    { id: 12, name: 'Vision'},
+    { id: 13, name: 'Feiticeira Scarlate'}
+];
